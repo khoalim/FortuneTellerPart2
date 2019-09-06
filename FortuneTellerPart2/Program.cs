782d9de6f086b246dd1b3ae5bf8dcca72d9b49a1
Kim Hoalim <kthoalim@gmail.com>
2019-09-06 17:14:50 -0400

    Initial Commit

---
+++ FortuneTellerPart2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FortuneTellerPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retirement Years Acitivity
+
+            Console.WriteLine("How old are you?");
+            int retirementAge;
+            retirementAge = Convert.ToInt32(Console.ReadLine());
+
+            if (retirementAge % 2 == 0)
+            {
+                Console.WriteLine("You have 10 years until retirement");
+            }
+
+            else
+            {
+                Console.WriteLine("You have 11 years until retirement");
+            }
+               
+
+        }
+    }
+}
